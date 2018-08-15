880e26151bf4ada40c1d0b361b7c9e89fab243ea
seobi49 <seobi49@gmail.com>
2018-08-16 00:00:19 +0900

    커밋 테스트

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ var usersRouter = require('./routes/users');
 var app = express();
 
 // view engine setup
+// 정보 수정
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.engine('ejs', engine);
